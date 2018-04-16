b72925438b0d647afb7ec300414c5fc6353ed572
Alex Bozarth <ajbozart@us.ibm.com>
2018-04-16 15:09:45 -0700

    Working full stack weapp
    - image diplay
    - file upload
    - ml analysis
    
    TODOs:
    
    - selectable images
    - word cloud
    - lokk and feel
    
    Nice to Haves:
    
    - soft shutdown
    - temp image clean up
    - better image ordering
    - display alt captions

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
-import tornado.ioloop
-import tornado.web
+from os import listdir
+import requests
+import time
+import tornado.ioloop as ioloop
+import tornado.web as web
 
 
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(web.RequestHandler):
     def get(self):
-        self.render("index.html")
+        self.render("index.html", image_captions=image_captions)
 
 
-class UploadHandler(tornado.web.RequestHandler):
+class UploadHandler(web.RequestHandler):
     def post(self):
-        file = self.request.files['file'][0]
-        file_name = file['filename']
-        output_file = open("static/img/MAX-" + file_name, 'wb')
-        output_file.write(file['body'])
-        self.finish("file " + file_name + " added")
+        file_des = self.request.files['file'][0]
+        file_name = "MAX-" + file_des['filename']
+        rel_path = "static/img/" + file_name
+        output_file = open(rel_path, 'wb')
+        output_file.write(file_des['body'])
+        output_file.close()
+        caption = run_ml(rel_path)
+        self.finish({"file_name": rel_path, "caption": caption})
+
+
+image_captions = {}
+
+
+def run_ml(img_path):
+    ml_endpoint = "http://localhost:5000/model/predict"
+
+    img_files = {'image': open(img_path, 'rb')}
+
+    r = requests.post(url=ml_endpoint, files=img_files)
+
+    cap_json = r.json()
+
+    caption = cap_json['predictions'][0]['caption']
+
+    image_captions[img_path] = caption
+
+    return caption
+
+
+def prepare_metadata():
+    # Create list of images with relative paths
+    rel_path = "static/img/"
+    image_list = listdir(rel_path)
+    rel_img_list = [rel_path + s for s in image_list]
+
+    # Run images through ML
+    for img in rel_img_list:
+        run_ml(img)
 
 
 def make_app():
@@ -28,12 +64,24 @@ def make_app():
         'debug': True
     }
 
-    web_app = tornado.web.Application(handlers, **configs)
+    web_app = web.Application(handlers, **configs)
 
     return web_app
 
 
-if __name__ == "__main__":
+def main():
+    print("MAX WebApp: Setting up App")
     app = make_app()
+    if not image_captions:
+        print("MAX WebApp: Preparing ML Metadata")
+        start = time.time()
+        prepare_metadata()
+        end = time.time()
+        print("MAX WebApp: Metadata prepared in " + str(end - start) + " seconds")
     app.listen(8088)
-    tornado.ioloop.IOLoop.current().start()
+    print("MAX WebApp: Ctrl+C to kill")
+    ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
--- static/webapp.js
+++ static/webapp.js
@@ -1,8 +1,34 @@
-function testPrompt() {
-    if (confirm("Press a button!")) {
-        txt = "You pressed OK!";
-    } else {
-        txt = "You pressed Cancel!";
-    }
-    alert(txt)
+// javascript code for MAX webapp
+
+function add_thumbnails(data) {
+    img_object = "<img src='" + data["file_name"] + "' alt='" + data["caption"] + "' title='" + data["caption"] + "' >";
+    $("#thumbnails").prepend(img_object);
 }
+
+$(function() {
+
+    // Image upload form submit functionality
+    $('#img_upload').on('submit', function(data){
+        // Stop form from submitting normally
+        event.preventDefault();
+
+        // Get form data
+        var form = event.target;
+        var data = new FormData(form);
+
+        // perform file upload
+        $.ajax({
+            url: "/upload",
+            method: "post",
+            processData: false,
+            contentType: false,
+            data: data,
+            processData: false,
+            dataType: "json",
+            success: function(data) {
+                add_thumbnails(data);
+            }
+        })
+    })
+
+});
