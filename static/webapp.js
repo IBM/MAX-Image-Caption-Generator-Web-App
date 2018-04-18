62da696c52efd1b8c428bbd7818c0c4f546deaf5
Alex Bozarth <ajbozart@us.ibm.com>
2018-04-18 12:27:17 -0700

    Added a common and short words filter to the word cloud

--- static/webapp.js
+++ static/webapp.js
@@ -18,6 +18,9 @@ function get_words() {
 function get_word_entries() {
     var words = get_words();
 
+    // common words from https://bl.ocks.org/blockspring/847a40e23f68d6d7e8b5
+    var common = "poop,i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,whose,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,will,would,should,can,could,ought,i'm,you're,he's,she's,it's,we're,they're,i've,you've,we've,they've,i'd,you'd,he'd,she'd,we'd,they'd,i'll,you'll,he'll,she'll,we'll,they'll,isn't,aren't,wasn't,weren't,hasn't,haven't,hadn't,doesn't,don't,didn't,won't,wouldn't,shan't,shouldn't,can't,cannot,couldn't,mustn't,let's,that's,who's,what's,here's,there's,when's,where's,why's,how's,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,upon,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,say,says,said,shall";
+
     var word_count = {};
 
     if (words.length == 1){
@@ -25,7 +28,7 @@ function get_word_entries() {
     } else {
         words.forEach(function (word) {
             var word = word.toLowerCase();
-            if (word != "" && word.length > 1) {
+            if (word != "" && common.indexOf(word) == -1 && word.length > 2) {
                 if (word_count[word]) {
                     word_count[word]++;
                 } else {
