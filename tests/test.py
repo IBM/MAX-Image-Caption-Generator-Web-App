bb5295f493eda245134d67569049035c1599ac04
Alex Bozarth <ajbozart@us.ibm.com>
2018-08-17 18:37:19 -0700

    Added pytest for upload and updsted travis to run it
    The pytest was take from the Image Caption Generator and edited to work with the web app

---
+++ tests/test.py
@@ -0,0 +1,26 @@
+import pytest
+import pycurl
+import io
+import json
+
+
+def test_upload():
+    c = pycurl.Curl()
+    b = io.BytesIO()
+    c.setopt(pycurl.URL, 'http://localhost:8088/upload')
+    c.setopt(pycurl.HTTPHEADER, ['Accept:application/json', 'Content-Type: multipart/form-data'])
+    c.setopt(pycurl.HTTPPOST, [('file', (pycurl.FORM_FILE, "static/img/images/7-action-athlete-896567.jpg"))])
+    c.setopt(pycurl.WRITEFUNCTION, b.write)
+    c.perform()
+    assert c.getinfo(pycurl.RESPONSE_CODE) == 200
+    c.close()
+
+    response = b.getvalue()
+    response = json.loads(response)
+
+    assert response['file_name'] == 'static/img/images/MAX-7-action-athlete-896567.jpg'
+    assert response['caption'] == 'a baseball player swinging a bat at a ball'
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
