304f07c40efb33c98136d68612cdd530db946fc0
Alex Bozarth <ajbozart@us.ibm.com>
2018-08-17 18:55:35 -0700

    Cleaner flake-passing style

--- tests/test.py
+++ tests/test.py
@@ -5,15 +5,15 @@ import json
 
 
 def test_upload():
+    test_file = 'static/img/images/7-action-athlete-896567.jpg'
+    temp_file = 'static/img/images/MAX-7-action-athlete-896567.jpg'
+
     c = pycurl.Curl()
     b = io.BytesIO()
     c.setopt(pycurl.URL, 'http://localhost:8088/upload')
     c.setopt(pycurl.HTTPHEADER,
              ['Accept:application/json', 'Content-Type: multipart/form-data'])
-    c.setopt(pycurl.HTTPPOST,
-             [('file',
-               (pycurl.FORM_FILE,
-                'static/img/images/7-action-athlete-896567.jpg'))])
+    c.setopt(pycurl.HTTPPOST, [('file', (pycurl.FORM_FILE, test_file))])
     c.setopt(pycurl.WRITEFUNCTION, b.write)
     c.perform()
     assert c.getinfo(pycurl.RESPONSE_CODE) == 200
@@ -22,8 +22,7 @@ def test_upload():
     response = b.getvalue()
     response = json.loads(response)[0]
 
-    assert (response['file_name']
-            == 'static/img/images/MAX-7-action-athlete-896567.jpg')
+    assert response['file_name'] == temp_file
     assert response['caption'] == 'a baseball player swinging a bat at a ball'
 
 
